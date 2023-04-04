94381ba479e930d7d6897799f0ba254f7822f007
trankhoaminh <tranminhbvhp@gmail.com>
2023-04-04 14:50:30 +0700

    add SubItem

--- MenuItem/CustomMenuItem.cs
+++ MenuItem/CustomMenuItem.cs
@@ -10,15 +10,23 @@ namespace PnC_Insurance.MenuItem
 {
     public class CustomMenuItem
     {
-        public BaseVM ContentViewModels { get; private set; }
+        public BaseVM? ContentViewModels { get; private set; }
         public string Label { get; private set; }
         public PackIconKind IconKind { get; private set; }
+        public List<CustomSubItem>? CustomSubItems { get; private set; }
 
-        public CustomMenuItem(BaseVM contentViewModels, string label, PackIconKind iconKind)
+        public CustomMenuItem(BaseVM? contentViewModels, string label, PackIconKind iconKind)
         {
-            ContentViewModels = contentViewModels;
+            ContentViewModels = contentViewModels ?? new BaseVM();
             Label = label;
             IconKind = iconKind;
+        }
+
+        public CustomMenuItem(string label, PackIconKind iconKind, List<CustomSubItem> subItems)
+        {          
+            Label = label;
+            IconKind = iconKind;
+            CustomSubItems = subItems ?? new List<CustomSubItem>();
         }
     }
 }
---
+++ MenuItem/CustomSubItem.cs
@@ -0,0 +1,12 @@
+﻿using MaterialDesignThemes.Wpf;
+using PnC_Insurance.ViewModel;
+
+namespace PnC_Insurance.MenuItem
+{
+    public class CustomSubItem : CustomMenuItem
+    {
+        public CustomSubItem(BaseVM? contentViewModels, string label, PackIconKind iconKind = PackIconKind.None) : base(contentViewModels, label, iconKind)
+        {
+        }
+    }
+}
