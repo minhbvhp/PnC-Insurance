17148876ccbabb14f07840103e4a9eeccdb27484
Minh Trần <tranminhbvhp@gmail.com>
2023-04-11 16:21:15 +0700

    add NewEmployee

--- ViewModel/UrnCreateVM.cs
+++ ViewModel/UrnCreateVM.cs
@@ -107,6 +107,94 @@ namespace PnC_Insurance.ViewModel
         }
         #endregion
 
+        #region New Employee
+        [ObservableProperty]
+        [Required(ErrorMessage = "Nhập số URN")]
+        [NotifyDataErrorInfo]
+        [NotifyCanExecuteChangedFor(nameof(AddNewEmployeeCommand))]
+        private long? newEmployeeUrn;
+
+        [ObservableProperty]
+        [Required(ErrorMessage = "Nhập tên nhân viên")]
+        [NotifyDataErrorInfo]
+        [NotifyCanExecuteChangedFor(nameof(AddNewEmployeeCommand))]
+        private string? newEmployeeName;
+
+        [ObservableProperty]
+        [Required(ErrorMessage = "Chọn Phòng")]
+        [NotifyDataErrorInfo]
+        [NotifyCanExecuteChangedFor(nameof(AddNewEmployeeCommand))]
+        private Department? departmentOfEmployee;
+
+        [RelayCommand(CanExecute = nameof(CanAddNewEmployeeCommand))]
+        private async Task AddNewEmployeeAsync()
+        {
+            ResultNotification = new SnackbarMessageQueue(TimeSpan.FromSeconds(3));
+
+            Employee addingEmployee = new Employee()
+            {
+                Urn = NewEmployeeUrn,
+                FullName = NewEmployeeName,
+                
+
+            };
+
+            var existDepartment = await Task.Run(() =>
+            {
+                using (var context = new InsuranceDbContext())
+                {
+                    var query = from department in context.Departments
+                                where department.Urn == addingDepartment.Urn
+                                select department;
+
+                    return query.ToList();
+
+                }
+            }
+            );
+
+            string notificationString = "";
+
+            if (existDepartment.Any())
+            {
+                notificationString = "Số URN này đã tồn tại";
+            }
+            else
+            {
+                notificationString = await Task.Run(async () =>
+                {
+                    using (var context = new InsuranceDbContext())
+                    {
+                        await context.Departments.AddAsync(addingDepartment);
+                        await context.SaveChangesAsync();
+                    }
+
+                    return "Đã tạo Phòng mới";
+                });
+
+                StartDeptOver();
+                OnPropertyChanged(nameof(ListOfDepartments));
+            }
+
+            ResultNotification.Enqueue(notificationString);
+        }
+
+        private bool CanAddNewEmployeeCommand()
+        {
+            if (NewDeptUrn != null && NewDeptName != null)
+                return true;
+
+            return false;
+        }
+
+        private void StartDeptOver()
+        {
+            NewDeptUrn = null;
+            NewDeptName = null;
+            ValidateAllProperties();
+        }
+        #region
+
         private void StartAgentOver()
         {
             
