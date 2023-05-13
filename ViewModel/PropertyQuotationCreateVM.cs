0b21e293c1cc25082703445c5d530fc8a9de10dd
Minh Trần <tranminhbvhp@gmail.com>
2023-05-13 17:52:17 +0700

    add Customer select feature

--- ViewModel/PropertyQuotationCreateVM.cs
+++ ViewModel/PropertyQuotationCreateVM.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.EntityFrameworkCore;
+using PnC_Insurance.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,40 @@ namespace PnC_Insurance.ViewModel
 {
     public partial class PropertyQuotationCreateVM : BaseVM
     {
+        [NotifyPropertyChangedFor(nameof(ListOfCustomers))]
+        [ObservableProperty]
+        private string? customerSearch;
+        public List<Customer>? ListOfCustomers
+        {
+            get
+            {
+                if (!String.IsNullOrEmpty(CustomerSearch) && !String.IsNullOrWhiteSpace(CustomerSearch))
+                {
+                    using (var context = new InsuranceDbContext())
+                    {
+                        var query = from customer in context.Customers.AsNoTracking()
+                                    where customer.IsDeleted == 0 &&
+                                          (EF.Functions.Like(customer.TaxCode, "%" + CustomerSearch + "%") ||
+                                          EF.Functions.Like(customer.Name, "%" + CustomerSearch + "%") ||
+                                          EF.Functions.Like(customer.Business, "%" + CustomerSearch + "%"))
+                                    orderby customer.Id
+                                    select customer;
+
+                        if (query.Any())
+                        {
+                            return query.ToList();
+                        }
+                    }
+                }
+
+                return new List<Customer>();
+            }
+
+        }
+
+        [ObservableProperty]
+        private Customer? selectedCustomer;
+
+
     }
 }
